71f347b5bd01b1481360dd742542e939a16a95ae
Garufi, Paola <paola.garufi@ge.com>
2017-07-03 19:15:58 -0700

    GWC2

---
+++ GWC/check3.js
@@ -0,0 +1,15 @@
+function check(form)
+{
+              if(form.userid.value == "username" && form.pswrd.value == "myp@$$wordi$100%$ecure") {window.open('Binary.html', "_self")}
+              else {alert("Is my password 100% secure??")}
+}
+
+
+
+
+
+function check_code(form)
+{
+              if(form.userid.value == "099 121 098 101 114 032 115 101 099 117 114 105 116 121") {window.open('safemail.html', "_self")}
+              else {alert("Enough Binary, give me ASCII!!")}
+}
